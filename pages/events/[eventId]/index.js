abb16d2990b1a8e949c923ca6fb02adc654a9893
griffin-a <griffinalmquist@gmail.com>
2022-07-07 17:11:05 +1200

    Boiler fixed for individual event's pages

--- pages/events/[eventId]/index.js
+++ pages/events/[eventId]/index.js
@@ -8,6 +8,10 @@ export default function EventId() {
           </h1>
         </div>
       </section>
+      <section class="bg-gray-100">
+          <div class="max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8">
+          </div>
+        </section>
     </div>
   );
 }
--- pages/events/[eventId]/photos/index.js
+++ pages/events/[eventId]/photos/index.js
@@ -7,6 +7,9 @@ export default function EventIdPhotos() {
             Event id photos page
           </h1>
         </div>
+        <section class="bg-gray-100">
+          <div class="max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8"></div>
+        </section>
       </section>
     </div>
   );
