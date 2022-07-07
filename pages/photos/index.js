f4b29e1dcd5a231c3f63963861c592ff7d892806
griffin-a <griffinalmquist@gmail.com>
2022-07-07 15:54:54 +1200

    Boiler plate for photos event selection

--- pages/photos/index.js
+++ pages/photos/index.js
@@ -2,6 +2,7 @@ import PaginationBoxes from "../../components/PaginationBoxes";
 import PaginationTab from "../../components/PaginationTab";
 import PhotoPage from "../../components/PhotoPage";
 import PhotoTopNav from "../../components/PhotoTopNav";
+import EventCardCTA from "../../components/EventCardCTA";
 
 export default function Photos() {
   return (
@@ -13,14 +14,23 @@ export default function Photos() {
           </h1>
         </div>
         <section class="bg-gray-100">
+          <div className="max-w-xl pt-7 mx-auto text-center">
+            <h2 className="text-black text-4xl font-bold tracking-tight sm:text-5xl">
+              Select an event
+            </h2>
+
+            <p className="max-w-lg mx-auto mt-4 text-gring-offset-warm-gray-500">
+              Please select an event from the list of events to view photos.
+            </p>
+          </div>
           <div class="max-w-screen-xl px-4 mx-auto sm:px-6 lg:px-8">
-            <div class="display: inline-block">
-              {/* <PhotoTopNav /> */}
-              <PhotoPage />
+            <div class="display: grid lg:grid-cols-2 py-10 px-4 gap-x-8 sm:grid-cols-1 sm:gap-y-8">
+              {/* <PhotoPage /> */}
+              <EventCardCTA />
+              <EventCardCTA />
             </div>
-            {/* <footer class="text-right">
-              <PaginationBoxes />
-            </footer> */}
+
+            <PaginationBoxes />
           </div>
         </section>
       </section>
