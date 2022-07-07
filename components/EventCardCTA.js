8d7d537855a986fda532faac85dc7ea5ba74a4b4
griffin-a <griffinalmquist@gmail.com>
2022-07-07 16:02:03 +1200

    Turning photo event card into component

--- components/EventCardCTA.js
+++ components/EventCardCTA.js
@@ -1,16 +1,31 @@
-export default function EventCardCTA() {
+import Image from "next/image";
+
+export default function EventCardCTA({
+  eventLink = "#",
+  title = "Default",
+  description = "Default description goes here",
+  imageUri = "/images/party-1.jpg",
+  backgroundColor = "bg-red-700",
+}) {
   return (
-    <a href="" class="relative flex items-end w-full bg-black h-96 group">
-      <img
-        alt="Modded Bike"
-        src="https://images.unsplash.com/photo-1605008585816-03ab23ce80af"
-        class="absolute inset-0 object-cover w-full h-full transition-opacity group-hover:opacity-90"
+    <a
+      href={eventLink}
+      className="relative flex items-end w-full bg-black h-96 group"
+    >
+      <Image
+        alt={title}
+        src={imageUri}
+        layout="fill" // required
+        objectFit="cover" // change to suit your needs
+        className="absolute inset-0 object-cover w-full h-full transition-opacity group-hover:opacity-90"
       />
 
-      <div class="relative w-full p-6 tracking-widest text-center text-white transition-colors bg-red-700 sm:w-2/3 group-hover:bg-black">
-        <strong class="text-lg uppercase">Custom Shop</strong>
+      <div
+        className={`relative w-full p-6 tracking-widest text-center text-white transition-colors ${backgroundColor} sm:w-2/3 group-hover:bg-black`}
+      >
+        <strong className="text-lg uppercase">{title}</strong>
 
-        <p class="mt-1 text-xs font-medium uppercase">Design your own</p>
+        <p className="mt-1 text-xs font-medium uppercase">{description}</p>
       </div>
     </a>
   );
