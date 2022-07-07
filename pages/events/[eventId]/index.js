93bebab5cc636a406f0cd24929b049350e53bd2c
griffin-a <griffinalmquist@gmail.com>
2022-07-07 17:34:13 +1200

    Event page boilerplate done

--- pages/events/[eventId]/index.js
+++ pages/events/[eventId]/index.js
@@ -1,17 +1,22 @@
+import CTASideImages from "../../../components/CTASideImages";
+import ReviewsGrid from "../../../components/ReviewsGrid";
+
 export default function EventId() {
   return (
     <div>
-      <section class="text-gray-600 body-font">
-        <div class="max-w-5xl pt-7 mx-auto">
-          <h1 class="text-5xl text-center font-4 lh-6 ld-04 font-bold text-white mb-6">
+      <section className="text-gray-600 body-font">
+        <div className="max-w-5xl pt-7 mx-auto">
+          <h1 className="text-5xl text-center font-4 lh-6 ld-04 font-bold text-white mb-6">
             Event id page
           </h1>
         </div>
       </section>
-      <section class="bg-gray-100">
-          <div class="max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8">
-          </div>
-        </section>
+      <section className="bg-gray-100">
+        <div className="pb-10">
+          <CTASideImages />
+          <ReviewsGrid />
+        </div>
+      </section>
     </div>
   );
 }
