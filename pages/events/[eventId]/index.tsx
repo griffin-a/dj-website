109a70aa5c328ebff3ef5e202b50f09956e999a5
griffin-a <griffinalmquist@gmail.com>
2022-09-03 12:20:27 +1200

    Update index.tsx

--- pages/events/[eventId]/index.tsx
+++ pages/events/[eventId]/index.tsx
@@ -19,7 +19,7 @@ export default function EventId() {
   const { cache } = useSWRConfig();
   const router = useRouter();
   
-  const [eventData, setEventData] = useState<Event | Event[]>();
+  const [eventData, setEventData] = useState<Event>(null);
 
   useEffect(() => {
     const eventId = router.query.eventId;
@@ -35,8 +35,11 @@ export default function EventId() {
       <section className="text-gray-600 body-font">
         <div className="max-w-5xl pt-7 mx-auto">
           <h1 className="text-5xl text-center font-4 lh-6 ld-04 font-bold text-white mb-6">
-            {/* {currentEvent.title} */}
-            {/* {currentEvent && <>{currentEvent.title}</>} */}
+            {eventData && (
+              <>
+                {eventData.title}
+              </>
+            )}
           </h1>
         </div>
       </section>
